c4394035fbb3d0809f1089e03255b3dbff52af65
Djack1010 <giachi.iada@gmail.com>
2018-07-20 16:11:25 +0200

    Change permission for topNLabelCounter.sh to run in workhorse

--- java.lang.NullPointerException/CGMM/test4_lightALL/topNLabelCounter.sh
+++ java.lang.NullPointerException/CGMM/test4_lightALL/topNLabelCounter.sh
@@ -79,10 +79,11 @@ else
     RPH="SET"
 fi
 
-rm -f /var/lock/lockCounterIDE${PIDRUN}
-touch /var/lock/lockCounterIDE${PIDRUN}
-rm -f /var/lock/lockCounterLIT${PIDRUN}
-touch /var/lock/lockCounterLIT${PIDRUN}
+mkdir -p ~/.mylock
+rm -f ~/.mylock/lockCounterIDE${PIDRUN}
+touch ~/.mylock/lockCounterIDE${PIDRUN}
+rm -f ~/.mylock/lockCounterLIT${PIDRUN}
+touch ~/.mylock/lockCounterLIT${PIDRUN}
 
 #PIDRUN=$$
 # echo -e "\033[2A"
@@ -142,7 +143,7 @@ function counterSubProcess {
                     echo -e $TLABEL"_1" >> $2/CounterIDE
                     #echo "NEW!"
                 fi
-            ) 200>/var/lock/lockCounterIDE$3
+            ) 200>~/.mylock/lockCounterIDE$3
         elif [ "$(echo "$l" | grep " LIT_")" ]; then
             TLABEL=$(echo "$l" | cut -d' ' -f2 )
             TEMPLINE4GREP=$(sed 's/[^^]/[&]/g; s/\^/\\^/g' <<<"$TLABEL")
@@ -163,7 +164,7 @@ function counterSubProcess {
                     echo -e $TLABEL"_1" >> $2/CounterLIT
                     #echo "NEW!"
                 fi
-            ) 200>/var/lock/lockCounterLIT$3
+            ) 200>~/.mylock/lockCounterLIT$3
         else
             continue
         fi
@@ -173,7 +174,7 @@ function counterSubProcess {
 FTOT=$(find $SCRIPTPATH/$INPUTFOLDER -name "*.$ARG4" | wc -l)
 FNOW=0
 #------------SET VARIABLE-------------
-MAXJOBS=6
+MAXJOBS=4
 UPDATE=1
 #-------------------------------------
 if [ "$CPH" ]; then
